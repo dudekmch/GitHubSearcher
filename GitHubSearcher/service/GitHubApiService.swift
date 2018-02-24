916416c768c874191516f3202e769f1332069dee
dudekmch <paweldominikdudek@gmail.com>
2018-02-24 13:33:50 +0100

    Revert "add GitHub api service"

--- GitHubSearcher/service/GitHubApiService.swift
+++
@@ -1,5 +0,0 @@
-import Alamofire
-
-class GitHubApiService {
-    
-}
--- Pods/Target Support Files/Pods-GitHubSearcher/Pods-GitHubSearcher-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GitHubSearcher : NSObject
-@end
-@implementation PodsDummy_Pods_GitHubSearcher
-@end
