15aff1265565e489b2243946a89c200f9bf16d1f
P.Dudek <paweldominikdudek@gmil.com>
2018-02-24 15:37:33 +0100

    Added method to create shadow

--- GitHubSearcher/commons/extensions/UIViewExtension.swift
+++ GitHubSearcher/commons/extensions/UIViewExtension.swift
@@ -1,9 +1,23 @@
-//
-//  UIViewExtension.swift
-//  GitHubSearcher
-//
-//  Created by Pawel Dudek on 24.02.2018.
-//  Copyright © 2018 cookieIT. All rights reserved.
-//
+import UIKit
 
-import Foundation
+extension UIView {
+
+    func addShadow(to side: ShadowSide) {
+        var offset: CGSize {
+            switch side {
+            case .bottom:
+                return CGSize(width: 0, height: -2)
+            case .top:
+                return CGSize(width: 0, height: 2)
+            case .left:
+                return CGSize(width: -2, height: 0)
+            case .right:
+                return CGSize(width: 2, height: 0)
+            }
+        }
+        self.layer.shadowOffset = offset
+        self.layer.shadowOpacity = 0.9
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.masksToBounds = false
+    }
+}
