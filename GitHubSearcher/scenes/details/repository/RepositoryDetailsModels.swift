7609c87853c3978fc877799647672d7876a97eb1
P.Dudek <paweldominikdudek@gmil.com>
2018-02-25 12:22:31 +0100

    Add VIP structure for RepositoryDetails scene

---
+++ GitHubSearcher/scenes/details/repository/RepositoryDetailsModels.swift
@@ -0,0 +1,27 @@
+//
+//  RepositoryDetailsModels.swift
+//  GitHubSearcher
+//
+//  Created by Pawel Dudek on 25.02.2018.
+//  Copyright (c) 2018 cookieIT. All rights reserved.
+//
+
+import UIKit
+
+enum RepositoryDetails {
+    // MARK: Use cases
+    
+    enum Something {
+        struct Request {
+            
+        }
+        
+        struct Response {
+            
+        }
+        
+        struct ViewModel {
+            
+        }
+    }
+}
---
+++ GitHubSearcher/scenes/details/repository/RepositoryDetailsViewController.swift
@@ -0,0 +1,76 @@
+//
+//  RepositoryDetailsViewController.swift
+//  GitHubSearcher
+//
+//  Created by Pawel Dudek on 25.02.2018.
+//  Copyright (c) 2018 cookieIT. All rights reserved.
+//
+
+import UIKit
+
+protocol RepositoryDetailsDisplayLogic: class {
+    func displaySomething(viewModel: RepositoryDetails.Something.ViewModel)
+}
+
+class RepositoryDetailsViewController: UIViewController, RepositoryDetailsDisplayLogic {
+    var interactor: RepositoryDetailsBusinessLogic?
+    var router: (NSObjectProtocol & RepositoryDetailsRoutingLogic & RepositoryDetailsDataPassing)?
+    
+    // MARK: Object lifecycle
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    // MARK: Setup
+    
+    private func setup() {
+        let viewController = self
+        let interactor = RepositoryDetailsInteractor()
+        let presenter = RepositoryDetailsPresenter()
+        let router = RepositoryDetailsRouter()
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
+    
+    // MARK: Routing
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let scene = segue.identifier {
+            let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
+            if let router = router, router.responds(to: selector) {
+                router.perform(selector, with: segue)
+            }
+        }
+    }
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        doSomething()
+    }
+    
+    // MARK: Do something
+    
+    //@IBOutlet weak var nameTextField: UITextField!
+    
+    func doSomething() {
+        let request = RepositoryDetails.Something.Request()
+        interactor?.doSomething(request: request)
+    }
+    
+    func displaySomething(viewModel: RepositoryDetails.Something.ViewModel) {
+        //nameTextField.text = viewModel.name
+    }
+}
