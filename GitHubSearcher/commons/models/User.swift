03e4563c32fd81e487e4cd5f3e0d67ce30192771
P.Dudek <paweldominikdudek@gmil.com>
2018-02-26 17:53:17 +0100

    Refactor User model

--- GitHubSearcher/commons/models/User.swift
+++ GitHubSearcher/commons/models/User.swift
@@ -7,25 +7,29 @@ class User {
     let score: Double
     
     let avatarURL: URL?
-    let userURL: URL?
-    let followersURL: URL?
-    let subscriptionsURL: URL?
-    let organizationsURL: URL?
-    let repositoriesURL: URL?
+    let userURL: URL
+    let followersURL: URL
+    let subscriptionsURL: URL
+    let organizationsURL: URL
+    let repositoriesURL: URL
     
-    init(login: String, id: Int, score: Double, avatarURL: URL?, userURL: URL, followersURL: URL, subscriptionsURL: URL, organizationsURL: URL, repositoriesURL: URL) {
+    init(login: String, id: Int, score: Double, avatarURL: String?, userURL: String, followersURL: String, subscriptionsURL: String, organizationsURL: String, repositoriesURL: String) {
         self.login = login
         self.id = id
         self.score = score
-        self.avatarURL = avatarURL
-        self.userURL = userURL
-        self.followersURL = followersURL
-        self.subscriptionsURL = subscriptionsURL
-        self.organizationsURL = organizationsURL
-        self.repositoriesURL = repositoriesURL
+        if let avatar = avatarURL {
+            self.avatarURL =  URL.init(string: avatar)
+        } else {
+            self.avatarURL = nil
+        }
+        self.userURL = URL.init(string: userURL)!
+        self.followersURL = URL.init(string: followersURL)!
+        self.subscriptionsURL = URL.init(string: subscriptionsURL)!
+        self.organizationsURL = URL.init(string: organizationsURL)!
+        self.repositoriesURL = URL.init(string: repositoriesURL)!
     }
     
-    init?(json: JSON) {
+    convenience init?(json: JSON) {
         guard
             let login = json["login"] as? String,
             let id = json["id"] as? Int,
@@ -38,16 +42,8 @@ class User {
             let repositoriesURL = json["repos_url"] as? String
 
         else { return nil }
-        
-                self.login = login
-                self.id = id
-                self.score = score
-                self.avatarURL = URL.init(string: avatarURL as String)
-                self.userURL =  URL.init(string: userURL as String)
-                self.followersURL =  URL.init(string: followersURL as String)
-                self.subscriptionsURL =  URL.init(string: subscriptionsURL as String)
-                self.organizationsURL =  URL.init(string: organizationsURL as String)
-                self.repositoriesURL =  URL.init(string: repositoriesURL as String)
+
+        self.init(login: login, id: id, score: score, avatarURL: avatarURL, userURL: userURL, followersURL: followersURL, subscriptionsURL: subscriptionsURL, organizationsURL: organizationsURL, repositoriesURL: repositoriesURL)
     }
 
 }
