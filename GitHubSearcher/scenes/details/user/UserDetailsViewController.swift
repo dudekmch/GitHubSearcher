dccb97c52fe7e3bbd7a1661a6a19945090254d9b
P.Dudek <paweldominikdudek@gmil.com>
2018-03-06 21:43:23 +0100

    resize avatar image

--- GitHubSearcher/scenes/details/user/UserDetailsViewController.swift
+++ GitHubSearcher/scenes/details/user/UserDetailsViewController.swift
@@ -7,21 +7,21 @@ protocol UserDetailsDisplayLogic: class {
 class UserDetailsViewController: UIViewController, UserDetailsDisplayLogic {
     var interactor: UserDetailsBusinessLogic?
     var router: (NSObjectProtocol & UserDetailsRoutingLogic & UserDetailsDataPassing)?
-    
+
     // MARK: Object lifecycle
-    
+
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         setup()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
-    
+
     // MARK: Setup
-    
+
     private func setup() {
         let viewController = self
         let interactor = UserDetailsInteractor()
@@ -34,20 +34,9 @@ class UserDetailsViewController: UIViewController, UserDetailsDisplayLogic {
         router.viewController = viewController
         router.dataStore = interactor
     }
-    
-    // MARK: Routing
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let scene = segue.identifier {
-            let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-            if let router = router, router.responds(to: selector) {
-                router.perform(selector, with: segue)
-            }
-        }
-    }
-    
+
     // MARK: View lifecycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = false
@@ -55,31 +44,48 @@ class UserDetailsViewController: UIViewController, UserDetailsDisplayLogic {
         repositoriesTableView.delegate = self
         getUserDetails()
     }
-    
-    // MARK: Do something
-    
+
+
     @IBOutlet weak var avatarImageView: UIImageView!
-    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var login: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var repositoriesTableView: UITableView!
-    
+
+    var user: User?
+
     private func getUserDetails() {
         interactor?.getUser()
     }
-    
+
     func displayUserDetails(viewModel: UserDetails.Data.ViewModel) {
-        //nameTextField.text = viewModel.name
+        self.user = viewModel.user
+        prepareUserDetails()
     }
+
+    private func prepareUserDetails() {
+        guard let user = self.user else { return }
+        avatarImageView.image = resizeAvatar(image: user.avatarImage)
+        login.text = user.login
+        scoreLabel.text = user.score.formatDoubleToString(toPlaceRounded: 1)
+    }
+
+    private func resizeAvatar(image: UIImage?) -> UIImage? {
+        guard let image = image else { return nil }
+        let rect = CGRect.init(x: 0, y: 0, width: 250, height: 250)
+        let size = CGSize.init(width: 250, height: 250)
+        return image.resizeImage(rect: rect, size: size)
+    }
+
 }
 
 extension UserDetailsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 15
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
-    
-    
+
+
 }
